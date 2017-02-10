eeb2e06881ef934db90ad14e5341deb74e2b21cd
taska <push.default.matching>
2017-02-10 18:07:55 +0800

    增加模块代码

---
+++ config-client-eureka/src/main/java/com/huluohu/cloud/client/ConfigClientAppRunner.java
@@ -0,0 +1,13 @@
+package com.huluohu.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientAppRunner {
+  public static void main(String[] args) {
+    SpringApplication.run(ConfigClientAppRunner.class, args);
+  }
+}
